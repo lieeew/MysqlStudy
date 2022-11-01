52ab71ff5ab097d5f3b25d28032e11e30f33a38f
百花二月 <108205879+lieeew@users.noreply.github.com>
2022-11-01 12:13:03 +0800

    Add files via upload

---
+++ study/cnm.java
@@ -0,0 +1,12 @@
+package Day22.study;
+
+/**
+ * @Auther: QiuXinYu
+ * @Date: 2022/11/1 - 11 - 01
+ * @Description: Day22.study
+ */
+public class cnm {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
