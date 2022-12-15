c3ab7a1406714cc0efd6a6b4e3627d9cbd42fafe
百花二月 <108205879+lieeew@users.noreply.github.com>
2022-12-15 16:05:59 +0800

    Add files via upload

---
+++ sqlCode/test1.sql
@@ -0,0 +1,18 @@
+# 演示创建数据库
+# 利用指令创建一个数据库 如果不指定字符集和校验规则用默认的
+CREATE DATABASE hsp_dbo1;
+# 删除数据库
+DROP DATABASE hsp_dbo1;
+# 创建一个指定utf8字符集的hsp_db02数据库
+CREATE DATABASE hsp_dbo2 CHARACTER SET utf8 
+# 创建一个指定utf8的字符集的hsp_db03的数据库 带有校对规则
+CREATE DATABASE hsp_dbo3 CHARACTER SET utf8 COLLATE utf8_bin
+# 校对规则 utf8_bin区分大小写 默认utf8_general_ci不区分大小写
+
+#下面是一条查询sql, 
+# SELECT 查询
+# * 表示所有字段
+# FROM 从哪个表
+# WHERE 从哪个字段 NAME = 'tom' 查询名字为tom
+SELECT * FROM T1
+SELECT * FROM T1 WHERE NAME = 'tom'
